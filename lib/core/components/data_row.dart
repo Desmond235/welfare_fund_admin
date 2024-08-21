8d64217cf860bc6ad667f01ab79b7873d4f59440
Desmond <adabedesmond@gmail.com>
2024-08-21 15:19:09 -0700

    added data row

---
+++ lib/core/components/data_row.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+createTitleCell(String name, bool isEditMode, {required void Function(String?) onSaved} ) {
+  return DataCell(
+    isEditMode
+        ? TextFormField(
+            initialValue: name,
+            onSaved: onSaved ,
+            style: const TextStyle(fontSize: 14),
+          )
+        : Text(name),
+  );
+}
+ 
