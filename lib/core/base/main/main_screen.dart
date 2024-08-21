91fdf6bbd74ed02c64c7d8c6f0938e605f5c60cd
Desmond <adabedesmond@gmail.com>
2024-08-21 00:23:14 -0700

    base

---
+++ lib/core/base/main/main_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:welfare_fund_admin/core/base/main/main_page_provider.dart';
+import 'package:welfare_fund_admin/core/constants/constants.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MainPageProvider>(builder: (_, pageState, __) {
+      return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: mainSystemUiOverlayStyle(context),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              pageState.currentPage == 1 ? "Home" : "Transactions",
+            ),
+          ),
+          body: KMainPages[pageState.currentPage],
+        ),
+      );
+    });
+  }
+}
