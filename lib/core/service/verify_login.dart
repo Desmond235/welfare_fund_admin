e7dfbd48843f9ef1ee7b4695c1a79eba4f373051
Desmond <adabedesmond@gmail.com>
2024-08-24 03:33:48 -0700

    added verify login

---
+++ lib/core/service/verify_login.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:welfare_fund_admin/core/controls/snackbar.dart';
+
+class VerifyLogin {
+  static post(String username, String password, BuildContext context) async {
+    const loginUrl = 'http://localhost:3000/api/admin/login';
+
+    try {
+       final response = await http.post(
+      Uri.parse(loginUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(
+        {
+          'username': username,
+          'password': password,
+        },
+      ),
+    );
+     
+     if(!context.mounted){
+      return ;
+     }
+    if(response.statusCode ==200){
+      log('login successful');
+    } else{
+      snackBar(context, 'Username or password is incorrect');
+    }
+    } on Exception catch (e) {
+      snackBar(context, 'An error occurred: $e');
+    }
+   
+  }
+}
